44a4d7095b3f5aec4291ec97a6c9bfe2ca35c8ab
Gregory Arefyev <gregory@recom.live>
2019-04-26 23:59:46 +0000

    Explicitly casting integer settings

--- server.py
+++ server.py
@@ -3,14 +3,14 @@ from recommender import Server
 import os, sys
 
 Server(
-        os.getenv('RECOM_SERVER_HOST',              'localhost'),
-        os.getenv('RECOM_SERVER_PORT',              5000),
-        os.getenv('RECOM_SERVER_LOGFILE',           'var/log/server.log'),
-        os.getenv('RECOM_SERVER_PIDFILE',           'var/run/server.pid'),
-        os.getenv('RECOM_SERVER_QLIMIT',            10000),
-        os.getenv('RECOM_SERVER_DOCLIMIT',          2000),
-        os.getenv('RECOM_SERVER_PERSONLIMIT',       2000),
-        os.getenv('RECOM_SERVER_RECSLIMIT',         5),
-        os.getenv('RECOM_SERVER_INVALIDATEAFTER',   5),
+        os.getenv('RECOM_SERVER_HOST',                  'localhost'),
+        int(os.getenv('RECOM_SERVER_PORT',              5000)),
+        os.getenv('RECOM_SERVER_LOGFILE',               'var/log/server.log'),
+        os.getenv('RECOM_SERVER_PIDFILE',               'var/run/server.pid'),
+        int(os.getenv('RECOM_SERVER_QLIMIT',            10000)),
+        int(os.getenv('RECOM_SERVER_DOCLIMIT',          2000)),
+        int(os.getenv('RECOM_SERVER_PERSONLIMIT',       2000)),
+        int(os.getenv('RECOM_SERVER_RECSLIMIT',         5)),
+        int(os.getenv('RECOM_SERVER_INVALIDATEAFTER',   5)),
         ).command(sys.argv[1])
 
